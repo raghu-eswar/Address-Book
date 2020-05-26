3c812d7542f97ee1221dfe3657a99f1f357199d7
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-26 09:56:50 +0530

    Added feature to create new file.

--- src/main/java/adderssBook/AddressBookController.java
+++ src/main/java/adderssBook/AddressBookController.java
@@ -2,9 +2,25 @@ package adderssBook;
 
 import filesSystem.FileManager;
 
+import java.io.File;
+import java.io.IOException;
+
 public class AddressBookController {
 
     static final String PATH = "./src/main/resources/";
     FileManager fileManager;
 
+    public boolean createNewBook(String bookName) {
+        File newBook = new File(PATH+bookName+".json");
+        boolean isBookCreated = false;
+        try {
+            isBookCreated = newBook.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (isBookCreated) {
+
+        }
+        return isBookCreated;
+    }
 }
---
+++ src/test/java/adderssBook/AddressBookControllerTest.java
@@ -0,0 +1,21 @@
+package adderssBook;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AddressBookControllerTest {
+    static AddressBookController controller = new AddressBookController();
+
+    @Test
+    public void forGivenProperName_AddressBookController_ShouldCreateNewBook() {
+        boolean status = controller.createNewBook("test");
+        Assert.assertTrue(status);
+    }
+
+    @Test
+    public void forGivenDuplicateName_AddressBookController_ShouldNotCreateNewBook() {
+        boolean status = controller.createNewBook("error");
+        Assert.assertFalse(status);
+    }
+
+}
